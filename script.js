57e5f85f712f5ebe456f0ff4808a804f0ee12856
Mohit Pramanik <mohitpramanik548@gmail.com>
2024-02-29 17:54:41 +0530

    Handled size adjustment when screen size get reduced

--- script.js
+++ script.js
@@ -101,47 +101,62 @@ buttons.forEach((button) => {
 
         // for equal button
         else if (e.target.innerHTML == '=') {
+
+            // if equal button pressed when screen is empty it won't affect the screen
             if (show == '') {
                 result.innerHTML = '';
             }
+
             else {
-                // screen.style.fontSize = '35px';
-                if (result.innerHTML.length > 11) {
-                    result.style.fontSize = '38px';
-                    screen.style.fontSize = '33px';
-                }
-                else {
+                
+                    // Adjusting font size according to screen size as they change dynamically when '=' is pressed
                     if (window.screen.availWidth < 637 && window.screen.availWidth > 359) {
+                        if(result.innerHTML.length > 11) {
+                            result.style.fontSize = '21px';
+                        }
+                        else {
+                            result.style.fontSize = '40px';
+                        }
                         screen.style.fontSize = '18px'
-                        screen.style.paddingTop = '29px'
-                        result.style.fontSize = '40px';
-                        result.style.paddingRight = '6px';
+                            screen.style.paddingTop = '29px'
+                            result.style.paddingRight = '6px';
                     }
+
                     else if (window.screen.availWidth < 360 && window.screen.availWidth > 274) {
                         screen.style.fontSize = '17px'
                         screen.style.paddingTop = '23px'
                         result.style.fontSize = '32px';
                         result.style.paddingRight = '6px';
                     }
+
                     else {
                         screen.style.fontSize = '35px';
                         result.style.fontSize = '82px';
                         result.style.paddingRight = '6px';
                     }
                 }
-            }
         }
 
         // for all number buttons
         else {
-            if ((calculator[calculator.length - 2] == '+' || calculator[calculator.length - 2] == '-' || calculator[calculator.length - 2] == '*' || calculator[calculator.length - 2] == '/' || calculator[calculator.length - 2] == '%') && calculator[calculator.length - 1] == '0') {
-                // if after operator 0 is entered and then again another number is entered then it will replace that 0
+
+            // if after operator, 0 is entered and then again another number is entered then it will replace that 0
+            if (
+                ((calculator[calculator.length - 2] == '+' ||
+                 calculator[calculator.length - 2] == '-' || 
+                 calculator[calculator.length - 2] == '*' || 
+                 calculator[calculator.length - 2] == '/' || 
+                 calculator[calculator.length - 2] == '%') && 
+                 calculator[calculator.length - 1] == '0') &&
+                 (e.target.innerHTML != '00')) { 
+
                 show = show.slice(0, show.length - 1) + e.target.innerHTML;
                 screen.innerHTML = show;
                 calculator = calculator.slice(0, calculator.length - 1) + e.target.innerHTML;
                 compute = eval(calculator);
-                result.innerHTML = compute;
+                result.innerHTML = '= ' + compute;
             }
+
             // if divided by zero
             else if (calculator[calculator.length - 1] == '/' && e.target.innerHTML == 0) {
                 show = show + e.target.innerHTML;
@@ -150,26 +165,23 @@ buttons.forEach((button) => {
                 result.innerHTML = '= Error';
             }
 
-            else if (screen.innerHTML == '0') {
-                show = e.target.innerHTML;
-                screen.innerHTML = show;
-                calculator = e.target.innerHTML;
-                compute = eval(calculator);
-                result.innerHTML = '= ' + compute;
+            // if screen is empty and '00' or '0' is pressed then it won't show any effect on screen except showing the result as 0
+            else if (screen.innerHTML == '0' && (e.target.innerHTML == '00' || e.target.innerHTML == '0')) {
+                    show = '0';
+                    screen.innerHTML = show;
+                    calculator = '0';
+                    compute = eval(calculator);
+                    result.innerHTML = '= ' + compute;                
             }
 
-
-            else if (screen.innerHTML == '0' && e.target.value == 00) {
-                show = '0';
-                screen.innerHTML = show;
-                calculator = '0';
-                compute = eval(calculator);
-                result.innerHTML = '= ' + compute;
-            }
-
-
-            else {
-                show = show + e.target.innerHTML;
+            // if screen is empty and any other no. other than '00' or '0' is pressed then the computation starts
+            else{
+                if(screen.innerHTML == '0') {
+                    show = e.target.innerHTML;
+                }
+                else {
+                    show = show + e.target.innerHTML;
+                }
                 screen.innerHTML = show;
                 calculator = calculator + e.target.innerHTML;
                 compute = eval(calculator);
@@ -179,19 +191,29 @@ buttons.forEach((button) => {
 
         // if screen element length become larger
         if (screen.innerHTML.length > 11) {
-            if (window.screen.availWidth < 637 && window.screen.availWidth > 359) {
+            if (window.innerWidth < 637 && window.innerWidth > 359) {
                 screen.style.fontSize = '23px'
-                result.style.fontSize = '19px';
             }
             else if (window.screen.availWidth < 360 && window.screen.availWidth > 274) {
                 screen.style.fontSize = '20px'
-                result.style.fontSize = '16px';
             }
-            else {
+            else{
                 screen.style.fontSize = '42px'
-                result.style.fontSize = '35px';
             }
             screen.style.overflow = 'scroll';
+        }
+
+        // if result element length become larger
+        if (result.innerHTML.length > 11) {
+            if (window.innerWidth < 637 && window.innerWidth > 359) {
+                result.style.fontSize = '19px';
+            }
+            else if (window.screen.availWidth < 360 && window.screen.availWidth > 274) {
+                result.style.fontSize = '16px';
+            }
+            else{
+                result.style.fontSize = '35px';
+            }
         }
     })
 })
